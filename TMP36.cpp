f7de30dc912f32ba1959a31ed348ab91ebbabc20
Derek Chafin <infomaniac50@gmail.com>
2011-09-14 21:45:28 -0500

    first commit

---
+++ TMP36.cpp
@@ -0,0 +1,38 @@
+/*
+  TMP36.cpp - Library for reading the TMP36 temperature sensor
+  Created by Derek Chafin, September 14, 2011
+  Released into the public domain.
+*/
+
+#include "WProgram.h"
+#include "TMP36.h"
+
+TMP36::TMP36(int tpin, float aref)
+{
+  _aref = aref;
+  _tpin = tpin;
+}
+
+void TMP36::update()
+{
+  int reading = analogRead(_tpin);
+  
+  float voltage = reading * _aref;
+  voltage /= 1024.0;
+  
+  //converting from 10 mv per degree with 500 mV offset
+  //to degrees ((volatge - 500mV) times 100)
+  _tempc = (voltage - 0.5) * 100 ;  
+  // now convert to Fahrenheight
+  _tempf = (_tempc * 9.0 / 5.0) + 32.0;   
+}
+
+float TMP36::getCelsius()
+{
+  return _tempc;
+}
+
+float TMP36::getFarenheight()
+{
+  return _tempf;
+}
---
+++ TMP36.h
@@ -0,0 +1,26 @@
+/*
+  TMP36.h - Library for reading the TMP36 temperature sensor
+  Created by Derek Chafin, September 14, 2011
+  Released into the public domain.
+*/
+
+#ifndef TMP36_h
+#define TMP36_h
+
+#include "WProgram.h"
+
+class TMP36
+{
+  public:
+    TMP36(int tpin, float aref);
+    void update();
+    float getCelsius();
+    float getFarenheight();
+  private:
+    int _tpin;
+    float _aref;      
+    float _tempf;
+    float _tempc;
+};
+
+#endif
